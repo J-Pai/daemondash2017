a04e39d54eab8549c029f15e25c0eca0b20c31cf
j-pai <jesse.pai@gmail.com>
2017-01-28 20:23:00 -0500

    Fixed redirect when not authorized

--- models/classroom.js
+++ models/classroom.js
@@ -18,98 +18,98 @@ db.once('open', function() {
 });
 
 var ClassroomSchema = new Schema({
-	room: {
-		type: String,
-		required: true,
-	},
-	building: {
-		type: String,
-		required: true,
-	},
-	locked: {
-		type: Date,
-	},
-	class: [{
-		department: String,
-		course: String,
-		section: String,
-		days: [String],
-		start: String,
-		end: String,
-	}],
-	reserved: [{
-		user: {
-			type : ObjectId,
-			ref : 'User',
-		},
-		day: String,
-		time: String,
-	}]
+    room: {
+        type: String,
+        required: true,
+    },
+    building: {
+        type: String,
+        required: true,
+    },
+    locked: {
+        type: Date,
+    },
+    class: [{
+        department: String,
+        course: String,
+        section: String,
+        days: [String],
+        start: String,
+        end: String,
+    }],
+    reserved: [{
+        user: {
+            type : ObjectId,
+            ref : 'User',
+        },
+        day: String,
+        time: String,
+    }]
 });
 
 ClassroomSchema.statics = _.merge(ClassroomSchema.statics, {
-	/**Reserves a room
-	 * @param {Object} opts - Parameters for the room to reserve
-	 * @param {String} opts.time - day and time that this will be reserved
-	 * @param {ObjectId} opts.user - user that is reserving this time slot
-	 * @param {String} opts.room - room to be reserved
-	 * @param {String} opts.building - building the room is in
-	*/
-	reserve : function(opts, cb) {
-		var Classroom = this;
-		Classroom.update({
-			building : opts.building,
-			room : opts.room
-		},
-		{
-			$addToSet : {
-				reserved : {
-					user : user,
-					day : opts.day,
-					time: opts.time,
-				}
-			}
-		}).exec(function(err) {
-			if (err) {
-				console.log(err);
-			}
-		});
-	},
+    /**Reserves a room
+     * @param {Object} opts - Parameters for the room to reserve
+     * @param {String} opts.time - day and time that this will be reserved
+     * @param {ObjectId} opts.user - user that is reserving this time slot
+     * @param {String} opts.room - room to be reserved
+     * @param {String} opts.building - building the room is in
+    */
+    reserve : function(opts, cb) {
+        var Classroom = this;
+        Classroom.update({
+            building : opts.building,
+            room : opts.room
+        },
+        {
+            $addToSet : {
+                reserved : {
+                    user : user,
+                    day : opts.day,
+                    time: opts.time,
+                }
+            }
+        }).exec(function(err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+    },
 
-	/**Removes reservation for a room
-	 * @param {Object} opts - Parameters for the room to remove reservation from 
-	 * @param {String} opts.user - user to remove from reservation
-	 * @param {String} opts.room - room to be reserved
-	 * @param {String} opts.building - building the room is in
-	*/
-	remove : function(opts, cb) {
-		var Classroom = this;
-		Classroom.update({
-			building: opts.building,
-			room : opts.room
-		}, 
-		{
-			$pull : {
-				user : opts.user
-			}
-		}).exec(function(err) {
-			if (err) {
-				console.log(err);
-			}
-		});
-	}
+    /**Removes reservation for a room
+     * @param {Object} opts - Parameters for the room to remove reservation from 
+     * @param {String} opts.user - user to remove from reservation
+     * @param {String} opts.room - room to be reserved
+     * @param {String} opts.building - building the room is in
+    */
+    remove : function(opts, cb) {
+        var Classroom = this;
+        Classroom.update({
+            building: opts.building,
+            room : opts.room
+        }, 
+        {
+            $pull : {
+                user : opts.user
+            }
+        }).exec(function(err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+    }
 
-	/**Gets top 3 rooms with longest times to return as a sms
-	 * @param {Object} opts - Parameters for this function 
-	 * @param {String} opts.building - building that you want to view rooms in
-	 * @param {String} opts.time - current time
-	*/
-	getTop : function(opts, cb) {
-		Classroom.find({
-			building: opts.building,
-			
-		})
-	}
+    /**Gets top 3 rooms with longest times to return as a sms
+     * @param {Object} opts - Parameters for this function 
+     * @param {String} opts.building - building that you want to view rooms in
+     * @param {String} opts.time - current time
+    */
+    getTop : function(opts, cb) {
+        Classroom.find({
+            building: opts.building,
+            
+        })
+    }
 
 });
 
